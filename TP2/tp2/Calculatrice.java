3e12df4a0b5524cb24e3d8d10824a8fb27b6e9b1
he201693 <he201693@D290.ephec.be>
2019-10-04 10:48:57 +0200

    ajout des constructeurs et setters/getters

---
+++ TP2/tp2/Calculatrice.java
@@ -0,0 +1,97 @@
+package tp2;
+
+/**
+ * Cette classe modélise une calculatrice simplifiée avec une valeur courante et trois opérations. 
+ * @author vvandens
+ *
+ */
+public class Calculatrice {
+
+	//variables
+	private double valeurCourante; // Stocke la valeur affichée sur l'�cran de la calculatrice
+
+
+
+
+	//setters et getters
+	/**
+	 * @return the valeurCourante
+	 */
+	public double getValeurCourante() {
+		return valeurCourante;
+	}
+
+
+	/**
+	 * @param valeurCourante the valeurCourante to set
+	 */
+	public void setValeurCourante(double valeurCourante) {
+		this.valeurCourante = valeurCourante;
+	}
+
+
+
+
+
+	//construct
+	/**
+	 * 
+	 */
+	public Calculatrice() {
+		//super(); //appelle le constructeur de hi�rarchie sup�rieur; pas tjs indispensable
+		valeurCourante = 10;
+	}
+
+
+	/**
+	 * @param valeurCourante
+	 */
+	public Calculatrice(double valeurCourante) {
+		super();
+		this.valeurCourante = valeurCourante;
+	}
+
+
+	//m�thodes
+	/**
+	 * Additionne un nombre à la valeur courante
+	 * @param n le nombre à ajouter à la valeur courante de la calculatrice
+	 */
+	public void ajoute(double n) {
+		valeurCourante+=n;
+	}
+
+
+
+	/**
+	 * Soustrait un nombre à la valeur courante
+	 * @param n le nombre à soustraire à la valeur courante de la calculatrice
+	 */
+	public void soustrait(double n) {
+		valeurCourante-=n;
+	}
+
+
+
+	/**
+	 * Elève la valeur courante au carré
+	 */
+	public void carre() {
+		valeurCourante*=valeurCourante;
+	}
+
+
+
+	/**
+	 * Méthode main permettant de lancer un test rapide de la classe calculatrice.
+	 * @param args les arguments de la ligne de commande
+	 */
+	public static void main(String[] args) {
+		Calculatrice myCalc = new Calculatrice();
+		System.out.println(myCalc.getValeurCourante());
+		myCalc.ajoute(5);
+		myCalc.soustrait(2);
+		System.out.println(myCalc.valeurCourante);
+	}
+
+}
